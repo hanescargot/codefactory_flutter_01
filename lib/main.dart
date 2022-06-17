c079279b20dc4471c1bf98e148c91235b9196fae
Hyunj Han <hyunjhan@Hyunjui-MacBookPro.local>
2022-06-17 18:33:32 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:first_app/Screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
